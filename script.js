c82a7d762df89fbe11512e90ca99717d1ed3abbd
abao929 <alexbao929@gmail.com>
2021-08-17 23:14:13 -0400

    sun animation

--- script.js
+++ script.js
@@ -8,18 +8,29 @@ function hideNav() {
     }
     prevScrollpos = currentScrollPos;
 }
-
-const links = document.querySelectorAll(".links");
-const sections = document.querySelectorAll("section");
-
-function changeLinkState() {
-  let index = sections.length;
-
-  while(--index && window.scrollY + 50 < sections[index].offsetTop) {}
-  
-  links.forEach((link) => link.classList.remove("active"));
-  links[index].classList.add("active");
-}
-
-window.addEventListener("scroll", changeLinkState);
 window.addEventListener("scroll", hideNav);
+
+function sunset() {
+  document.getElementById("sun").classList.add("sunsetClass");
+  document.getElementById("sun").classList.remove("hvr-sink");
+  document.body.classList.add("dark-fade");
+  document.getElementsByClassName("top-half")[0].classList.add("dark-fade");
+  document.getElementsByClassName("project-header")[0].classList.add("dark-fade");
+  document.getElementsByClassName("bot-half")[0].classList.add("darker-fade");
+  document.getElementsByClassName("about-container")[0].classList.add("darker-fade");
+  document.getElementsByClassName("description-container")[0].classList.add("darker-fade");
+  document.getElementsByClassName("description-container")[1].classList.add("darker-fade");
+  setTimeout(function(){
+    document.getElementById("sun").classList.add("hideSun");
+    document.getElementById("sun").classList.remove("showSun");
+    document.getElementById("sun").classList.remove("sunsetClass");
+    document.getElementById("me-image").src = "images/me-background-dark.png";
+    document.body.classList.add("dark");
+    document.getElementsByClassName("top-half")[0].classList.add("dark");
+    document.getElementsByClassName("project-header")[0].classList.add("dark");
+    document.getElementsByClassName("bot-half")[0].classList.add("darker");
+    document.getElementsByClassName("about-container")[0].classList.add("darker");
+    document.getElementsByClassName("description-container")[0].classList.add("darker");
+    document.getElementsByClassName("description-container")[1].classList.add("darker");
+  }, 1990);
+}
