3c2f9071224bdf3237d23e34e469486beab353a5
abao929 <alexbao929@gmail.com>
2021-08-19 15:01:24 -0400

    working sunset sunrise

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ function hideNav() {
 window.addEventListener("scroll", hideNav);
 
 function sunset() {
-  document.getElementById("sun").classList.add("sunsetClass");
+  document.getElementById("sun").classList.add("setClass");
   document.getElementById("sun").classList.remove("hvr-sink");
   document.body.classList.add("dark-fade");
   document.getElementsByClassName("top-half")[0].classList.add("dark-fade");
@@ -21,10 +21,24 @@ function sunset() {
   document.getElementsByClassName("description-container")[0].classList.add("darker-fade");
   document.getElementsByClassName("description-container")[1].classList.add("darker-fade");
   setTimeout(function(){
-    document.getElementById("sun").classList.add("hideSun");
-    document.getElementById("sun").classList.remove("showSun");
-    document.getElementById("sun").classList.remove("sunsetClass");
+    document.getElementById("sun").src = "images/moon.png";
+  }, 1990);
+  setTimeout(function(){
     document.getElementById("me-image").src = "images/me-background-dark.png";
+    document.body.classList.remove("light");
+    document.getElementsByClassName("top-half")[0].classList.remove("light");
+    document.getElementsByClassName("project-header")[0].classList.remove("light");
+    document.getElementsByClassName("bot-half")[0].classList.remove("lighter");
+    document.getElementsByClassName("about-container")[0].classList.remove("lighter");
+    document.getElementsByClassName("description-container")[0].classList.remove("lighter");
+    document.getElementsByClassName("description-container")[1].classList.remove("lighter");
+    document.body.classList.remove("dark-fade");
+    document.getElementsByClassName("top-half")[0].classList.remove("dark-fade");
+    document.getElementsByClassName("project-header")[0].classList.remove("dark-fade");
+    document.getElementsByClassName("bot-half")[0].classList.remove("darker-fade");
+    document.getElementsByClassName("about-container")[0].classList.remove("darker-fade");
+    document.getElementsByClassName("description-container")[0].classList.remove("darker-fade");
+    document.getElementsByClassName("description-container")[1].classList.remove("darker-fade");
     document.body.classList.add("dark");
     document.getElementsByClassName("top-half")[0].classList.add("dark");
     document.getElementsByClassName("project-header")[0].classList.add("dark");
@@ -32,5 +46,50 @@ function sunset() {
     document.getElementsByClassName("about-container")[0].classList.add("darker");
     document.getElementsByClassName("description-container")[0].classList.add("darker");
     document.getElementsByClassName("description-container")[1].classList.add("darker");
+    document.getElementById("sun").classList.remove("setClass");
+    document.getElementById("sun").classList.add("hvr-sink");
+    document.getElementById("sun").setAttribute("onclick", "sunrise()");
+  }, 3990);
+}
+
+function sunrise() {
+  document.getElementById("sun").classList.add("setClass");
+  document.getElementById("sun").classList.remove("hvr-sink");
+  document.body.classList.add("light-fade");
+  document.getElementsByClassName("top-half")[0].classList.add("light-fade");
+  document.getElementsByClassName("project-header")[0].classList.add("light-fade");
+  document.getElementsByClassName("bot-half")[0].classList.add("lighter-fade");
+  document.getElementsByClassName("about-container")[0].classList.add("lighter-fade");
+  document.getElementsByClassName("description-container")[0].classList.add("lighter-fade");
+  document.getElementsByClassName("description-container")[1].classList.add("lighter-fade");
+  setTimeout(function(){
+    document.getElementById("sun").src = "images/sun.png";
   }, 1990);
+  setTimeout(function(){
+    document.getElementById("me-image").src = "images/me-background.png";
+    document.body.classList.remove("dark");
+    document.getElementsByClassName("top-half")[0].classList.remove("dark");
+    document.getElementsByClassName("project-header")[0].classList.remove("dark");
+    document.getElementsByClassName("bot-half")[0].classList.remove("darker");
+    document.getElementsByClassName("about-container")[0].classList.remove("darker");
+    document.getElementsByClassName("description-container")[0].classList.remove("darker");
+    document.getElementsByClassName("description-container")[1].classList.remove("darker");
+    document.body.classList.remove("light-fade");
+    document.getElementsByClassName("top-half")[0].classList.remove("light-fade");
+    document.getElementsByClassName("project-header")[0].classList.remove("light-fade");
+    document.getElementsByClassName("bot-half")[0].classList.remove("lighter-fade");
+    document.getElementsByClassName("about-container")[0].classList.remove("lighter-fade");
+    document.getElementsByClassName("description-container")[0].classList.remove("lighter-fade");
+    document.getElementsByClassName("description-container")[1].classList.remove("lighter-fade");
+    document.body.classList.add("light");
+    document.getElementsByClassName("top-half")[0].classList.add("light");
+    document.getElementsByClassName("project-header")[0].classList.add("light");
+    document.getElementsByClassName("bot-half")[0].classList.add("lighter");
+    document.getElementsByClassName("about-container")[0].classList.add("lighter");
+    document.getElementsByClassName("description-container")[0].classList.add("lighter");
+    document.getElementsByClassName("description-container")[1].classList.add("lighter");
+    document.getElementById("sun").classList.remove("setClass");
+    document.getElementById("sun").classList.add("hvr-sink");
+    document.getElementById("sun").setAttribute("onclick", "sunset()");
+  }, 3990);
 }
